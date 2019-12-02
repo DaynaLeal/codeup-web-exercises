4a11c78833e9d6458544c3e2eec8f48caacdfb96
Dayna Leal <dayna.r.leal@gmail.com>
2019-12-02 10:50:11 -0600

    changing answers based on question to js with html exercises

--- js/external.js
+++ js/external.js
@@ -40,30 +40,41 @@ alert("Woah, " + usersFavColor + " is my favorite color too!");
 
 
 //3.1
-function movieRentals(movie1, movie2, movie3) {
-    return (3 * movie1) + (3 * movie2) + (3 * movie3);
-}
-console.log(movieRentals(3, 5, 1)); //returns 27
+alert("You want to rent some movies for your kids: The little mermaid, Brother Bear (they love it!), and Hercules (you don't know yet if they're going to like it). The price per movie rental per day is $3. Let's see how much you will need to pay.");
+var littleMermaid = prompt("How many nights do you want to rent The Little Mermaid?");
+var brotherBear = prompt("How many nights do you want to rent Brother Bear?");
+var herculesMovie = prompt("How many nights do you want to rent Hercules?");
+var movieRentals = (3 * littleMermaid) + (3 * brotherBear) + (3 * herculesMovie);
+alert("You will have to pay $" + movieRentals + ".");
+console.log(movieRentals);
 
-var userAnswerRentals = prompt("You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?");
+//below is based on what I originally thought this question was asking:
+// if (userAnswerRentals === 27 || "$27" || "$27.00") {
+//     alert("You're Correct! You would have to pay $27 for your movie rentals.");
 
-if (userAnswerRentals === 27 || "$27" || "$27.00") {
-    alert("You're Correct! You would have to pay $27 for your movie rentals.");
-}
 
 
 
 
 //3.2
-function weeklyPay(googleHours, amazonHours, facebookHours) {
-    return (400 * googleHours) + (380 * amazonHours) + (350 * facebookHours);
-}
-console.log(weeklyPay(6, 4, 10)); //returns 7420
 
-var userAnswerPay = prompt("Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.");
-if (userAnswerPay === 7420 || "$7420" || "$7,420" || "$7420.00" || "$7,420.00") {
-    alert("You're Correct! You would be paid $7,420 for your work.")
-}
+alert("You're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. Let's determine how much you made based on the amount of hours you worked this week.");
+var googleHours = prompt("How many hours did you work for Google this week?");
+var amazonHours = prompt("How many hours did you work for Amazon this week?");
+var facebookHours = prompt("How many hours did you work for Amazon this week?");
+var totalPay = (400 * googleHours) + (380 * amazonHours) + (350 * facebookHours);
+alert("You would be paid $" + totalPay + " for your work this week!" );
+console.log(totalPay);
+//below is based on what I originally thought this question was asking:
+// function weeklyPay(googleHours, amazonHours, facebookHours) {
+//     return (400 * googleHours) + (380 * amazonHours) + (350 * facebookHours);
+// }
+// console.log(weeklyPay(6, 4, 10)); //returns 7420
+//
+// var userAnswerPay = prompt("Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.");
+// if (userAnswerPay === 7420 || "$7420" || "$7,420" || "$7420.00" || "$7,420.00") {
+//     alert("You're Correct! You would be paid $7,420 for your work.")
+// }
 
 // 3.3
 //A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
