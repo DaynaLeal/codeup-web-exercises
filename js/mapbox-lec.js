9de4abd88b041d5e421a17419d53ddb3e40b07dc
Dayna Leal <dayna.r.leal@gmail.com>
2019-12-17 12:21:18 -0600

    mapbox lecture files

--- js/mapbox-lec.js
+++ js/mapbox-lec.js
@@ -1,10 +1,10 @@
 /**********************************************
- * 			SETTING UP KEYS.JS
+ *            SETTING UP KEYS.JS
  *********************************************/
 // TODO TOGETHER: Open .gitignore and add keys.js. Add keys.js file and import to mapbox html file. Your api keys are stored in keys.js and are added to the .gitignore so they are protected
 
 /**********************************************
- * 			CUSTOMIZING THE MAP
+ *            CUSTOMIZING THE MAP
  *********************************************/
 // Predefined map styles --> https://docs.mapbox.com/mapbox-gl-js/api/#map
 // A map center can be set by passing in the latitude and longitude coordinates of a location as an array [LONGITUDE_VALUE, LATITUDE_VALUE]
@@ -12,33 +12,143 @@
 
 //TODO TOGETHER: Set map to san antonio area using the coordinates [-98.4916, 29.4252]
 
+// Custom JS
+
+mapboxgl.accessToken = mapboxToken;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+        // 'mapbox://styles/mapbox/dark-v10',
+
+    zoom: 9,
+    center: [-98.4916, 29.4252], //longitude, latitude (commonly listed 'latitude, longitude')
+
+});
+
+geocode("600 Navarro St. San Antonio TX, 78205", mapboxToken).then(function(result){
+    console.log(result);
+    //recenter the map
+    map.setCenter(result);
+    //set a marker
+});
+geocode("445 Martinez Ln, Poteet TX, 78065", mapboxToken).then(function(result){
+    console.log(result);
+})
+
+
 //TODO: Experiment with different map styles, zoom levels, and centers. You will need to reference the mapbox docs. (~15 minutes)
 
 
 /**********************************************
- * 					MARKERS
+ *                    MARKERS
  *********************************************/
 // Marker Docs --> https://docs.mapbox.com/mapbox-gl-js/api/#marker
 // Markers are specific locations on a map
 //Use the .setLngLat() and .addTo() methods to add marker to the map
 
+    //to create points on map
+    //https://docs.mapbox.com/help/tutorials/add-points-pt-1/
+
 
 // TODO TOGETHER: Add a marker to the map using the following coordinates [-98.4916, 29.4260]. This marker will mark the Alamo on our map.
+
+var markerOptions = {
+    color: 'red',
+    // draggable: true,
+};
+
+var markerCodeup = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-98.489615, 29.426827])
+    .addTo(map)
+
+var markerHome = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-98.6467574, 29.0773323])
+    .addTo(map)
+
+markerCodeup.setPopup(popupCodeup);
+markerHome.setPopup(popupHome);
+
+
 // TODO TOGETHER: Change the color of the marker
 
 
 // TODO: Experiment with the color, and setting the LngLat
 // TODO: Update the marker object to make the marker draggable. *Hint: reference the docs!
 
+// The 'building' layer in the mapbox-streets vector source contains building-height
+// data from OpenStreetMap.
+//https://docs.mapbox.com/mapbox-gl-js/example/3d-buildings/
+
+map.on('load', function () {
+// Insert the layer beneath any symbol layer.
+    var layers = map.getStyle().layers;
+
+    var labelLayerId;
+    for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+        }
+    }
+
+    map.addLayer(
+        {
+            'id': '3d-buildings',
+            'source': 'composite',
+            'source-layer': 'building',
+            'filter': ['==', 'extrude', 'true'],
+            'type': 'fill-extrusion',
+            'minzoom': 15,
+            'paint': {
+                'fill-extrusion-color': '#aaa',
+
+// use an 'interpolate' expression to add a smooth transition effect to the
+// buildings as the user zooms in
+                'fill-extrusion-height': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'height']
+                ],
+                'fill-extrusion-base': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'min_height']
+                ],
+                'fill-extrusion-opacity': 0.6
+            }
+        },
+        labelLayerId
+    );
+});
+
 
 /**********************************************
- * 					POPUPS
+ *                    POPUPS
  *********************************************/
 // Popups are the info boxes that appear on a map and may describe a given location.
 // Popup docs --> https://docs.mapbox.com/mapbox-gl-js/api/#popup
 
 
 // TODO TOGETHER: Add a popup to the map over codeup. Set the html as a paragraph that says "Codeup Rocks!"
+
+var popupCodeup = new mapboxgl.Popup()
+    // .setLngLat([-98.489615, 29.426827]) //commented out to bind to the marker instead of putting the popup at the same spot
+    .setHTML("<p>Codeup is right here!</p>")
+    .addTo(map);
+
+var popupHome = new mapboxgl.Popup()
+    .setHTML("<p>I live all the way over here!</p>")
+    .addTo(map);
+
+
 // TODO TOGETHER: Comment out the popup we just added. Add a popup to the alamo marker.
 
 
@@ -48,7 +158,7 @@
 
 
 /**********************************************
- * 					Geocoder
+ *                    Geocoder
  *********************************************/
 // Geocoding Docs --> https://docs.mapbox.com/api/search/#geocoding
 
@@ -56,13 +166,44 @@
 // TODO TOGETHER: Using the Geocoder helper function, log the coordinates of Codeup and recenter the map to focus on Codeup. Comment out previous map code.
 
 
+// function geocode(search, token) {
+//     var baseUrl = 'https://api.mapbox.com';
+//     var endPoint = '/geocoding/v5/mapbox.places/';
+//     return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+//         .then(function(res) {
+//             return res.json();
+//             // to get all the data from the request, comment out the following three lines...
+//         }).then(function(data) {
+//             return data.features[0].center;
+//         });
+// }
+
+
 //TODO: Using the geocode method above, add a marker at Codeup to the map
 //TODO: Instead of setCenter try using map.jumpTo()
 //TODO: Instead of setCenter try using map.flyTo()
 
 
-
 // TODO TOGETHER: Reverse Geocoding: Using the reverse geocoding method, enter the coordinates {lng: -98.4861, lat: 29.4260} to get a physical address for the alamo
+
+// function reverseGeocode(coordinates, token) {
+//     var baseUrl = 'https://api.mapbox.com';
+//     var endPoint = '/geocoding/v5/mapbox.places/';
+//     return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+//         .then(function(res) {
+//             return res.json();
+//         })
+//         // to get all the data from the request, comment out the following three lines...
+//         .then(function(data) {
+//             return data.features[0].place_name;
+//         });
+// }
+
+reverseGeocode([-98.4861, 29.4260], mapboxToken).then(function(results){
+    console.log(results);
+});
+
+
 // TODO: Reverse geocode coordinates of your choice using the reverse geocode method
 
 
