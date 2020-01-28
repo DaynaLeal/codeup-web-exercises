a8d64dab30e8bb301d9ca5b5223908322316630c
Dayna Leal <dayna.r.leal@gmail.com>
2020-01-28 09:49:17 -0600

    double checking files

---
+++ js/keys.js
@@ -0,0 +1,5 @@
+var mapboxToken = "pk.eyJ1IjoiZGF5bmFsIiwiYSI6ImNrNDliN25ucTAzaXczZW8xNDk3d2tjanEifQ.JDfyhYNKKQJgj1lgBLiGXQ";
+
+var darkSkyKey = "95a5b09c346f33b96c7a61f32a83185a";
+
+var gitAccessToken = 'a30120b49030b79f48bc9703340bddbeb375caf2';
